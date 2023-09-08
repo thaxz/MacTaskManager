56dd79728dce11b0d45f5f63d235cda181223e9e
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-09-08 19:29:06 -0300

    feature(CoreData): Adding filters to search tasks

--- TaskManager/TaskManager/ContentView.swift
+++ TaskManager/TaskManager/ContentView.swift
@@ -13,22 +13,28 @@ struct ContentView: View {
     @State private var userCreatedGroups: [TaskGroup] = TaskGroup.examples()
     @State private var allTasks = Task.examples()
     
+    @State private var searchTerm: String = ""
+    
     var body: some View {
         NavigationSplitView {
             SideBarView(userCreatedGroups: $userCreatedGroups, selection: $selection)
         } detail: {
-            switch selection {
-            case .all:
-                TaskListView(title: "All", tasks: $allTasks)
-            case .done:
-                StaticTaskListView(title: "Done", tasks: allTasks.filter({$0.isCompleted}))
-            case .upcoming:
-                StaticTaskListView(title: "Upcoming", tasks: allTasks.filter({!$0.isCompleted}))
-            case .list(let taskGroup):
-                StaticTaskListView(title: taskGroup.title, tasks: taskGroup.tasks)
+            if searchTerm.isEmpty {
+                switch selection {
+                case .all:
+                    TaskListView(title: "All", tasks: $allTasks)
+                case .done:
+                    StaticTaskListView(title: "Done", tasks: allTasks.filter({$0.isCompleted}))
+                case .upcoming:
+                    StaticTaskListView(title: "Upcoming", tasks: allTasks.filter({!$0.isCompleted}))
+                case .list(let taskGroup):
+                    StaticTaskListView(title: taskGroup.title, tasks: taskGroup.tasks)
+                }
+            } else {
+                StaticTaskListView(title: "Done", tasks: allTasks.filter({$0.title.contains(searchTerm)}))
             }
         }
-
+        .searchable(text: $searchTerm)
     }
 }
 
