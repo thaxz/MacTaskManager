e0bf213543194a1cd0544e26c42fa756b18fcb66
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-09-08 18:16:41 -0300

    feature(CoreData): fix/project-swiftUi

---
+++ TaskManager/TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by thaxz on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
